8931f7b10ab2c0ca326768afc1e57deaf895d8ae
Evam Baker <ecbaker3@bsu.edu>
2017-02-10 17:38:52 -0500

    Created a sample JavaFx Interface.

--- src/edu/bsu/cs222/twp/UI/Controller.java
+++ src/edu/bsu/cs222/twp/UI/Controller.java
@@ -1,4 +1,10 @@
 package edu.bsu.cs222.twp.UI;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    //Super Special Awesome Mega Duper Luper Method
+    public void getWikipediaPageChanges(ActionEvent actionEvent) {
+
+    }
 }
