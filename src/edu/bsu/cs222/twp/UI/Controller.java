49fd1e620aa5ceda64eabc88088c54d9ac8b78e3
Evam Baker <ecbaker3@bsu.edu>
2017-02-12 17:10:35 -0500

    Assigned the procces to a button.

--- src/edu/bsu/cs222/twp/UI/Controller.java
+++ src/edu/bsu/cs222/twp/UI/Controller.java
@@ -4,11 +4,14 @@ import edu.bsu.cs222.twp.parser.RevisionParser;
 import edu.bsu.cs222.twp.url.URLConnector;
 import edu.bsu.cs222.twp.url.URLConstructor;
 import edu.bsu.cs222.twp.xml.XMLBuilder;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import org.xml.sax.SAXException;
 
+import javax.swing.text.html.ListView;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +20,9 @@ import java.net.URL;
 public class Controller {
     public TextField userURL = new TextField();
     public InputStream XMLStream;
+    public javafx.scene.control.ListView listOfNamesAndTimes;
+    public Button bigRedButton;
+
 
     //Super Special Awesome Mega Duper Luper Method == the big red button that does everything
     public void getWikipediaPageChanges(ActionEvent actionEvent) throws IOException, ParserConfigurationException, SAXException {
@@ -30,10 +36,7 @@ public class Controller {
         RevisionParser revisions = new RevisionParser();
 
 
-        revisions.parse(XMLStream);
-
-
-
+        listOfNamesAndTimes.setItems((ObservableList) revisions.parse(XMLStream));
 
 
     }
